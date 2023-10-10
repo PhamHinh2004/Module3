5ba6071d6acb2f681778e123980f9cdabaf60197
Phạm V.Hinh <137275695+PhamHinh2004@users.noreply.github.com>
2023-10-10 22:38:40 +0700

    Da them code o module  3 bo sung o pakage hanghoa

---
+++ modurn3/src/HangHoa/DanhSachHangHoa.java
@@ -0,0 +1,12 @@
+package HangHoa;
+
+public class DanhSachHangHoa {
+	protected HangHoa hh[];
+
+	public DanhSachHangHoa(int ptu) {
+		hh = new HangHoa[ptu];
+	}
+	public void themHangHoa(HangHoa hhValue) {
+		if(hhValue.getMaHang())
+	}
+}
